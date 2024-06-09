3879e1c97d940069c732fa5306e001930025dc56
Robiul Awal <robiulawal527@gmail.com>
2024-06-09 13:42:34 +0600

    Lab Class 1 Practice Problems

---
+++ 242_OOP(E)/Lab1/Problem3/Problem3.java
@@ -0,0 +1,11 @@
+package Problem3;
+
+public class Problem3 {
+    public static void main(String[] args) {
+        int a = 1, b = 2, c = 3;
+        System.out.println(a + "    " + a*a + "     " +  a*a*a);
+        System.out.println(b + "        " + b*b + "     " +  b*b*b);
+        System.out.println(c + "       " + c*c + "     " +  c*c*c);
+        
+    }
+}
---
+++ 242_OOP(E)/Lab1/Problem_12/Main.java
@@ -0,0 +1,9 @@
+package Problem_12;
+
+//import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        
+    }
+}
