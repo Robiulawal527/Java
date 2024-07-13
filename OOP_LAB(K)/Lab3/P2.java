2477a54a1f1b791a02b7161002330f5ef4cd2425
Robiulawal527 <robiulawal527@gmail.com>
2024-07-13 20:34:42 +0600

    gcd

--- OOP_LAB(K)/Lab3/P2.java
+++ OOP_LAB(K)/Lab3/P2.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
 public class P2 {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter Second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("gcd of two number is: " + gcd(a, b));
+        sc.close();
         
     }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+    
 }
