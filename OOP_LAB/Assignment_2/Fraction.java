97bd199c0aa946bebb86a50bfc0a2763ac0786e4
Robiulawal527 <robiulawal527@gmail.com>
2024-07-17 15:18:17 +0600

    chore: Add Javadoc comments to Fraction class and TestFraction class

--- OOP_LAB/Assignment_2/Fraction.java
+++ OOP_LAB/Assignment_2/Fraction.java
@@ -1,5 +1,7 @@
-//package Assignment_2;
-
+/**
+ * The Fraction class represents a fraction with a numerator and a denominator.
+ * It provides methods for performing arithmetic operations on fractions.
+ */
 public class Fraction {
     private int numerator;
     private int denominator;
@@ -17,6 +19,15 @@ public class Fraction {
         this.numerator = numerator;
     }
 
+    /**
+     * Sets the numerator and denominator of the fraction.
+     */
+    public void setFraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    
     public int getDenominator() {
         return denominator;
     }
@@ -40,24 +51,34 @@ public class Fraction {
         this.denominator=this.denominator*fraction.denominator;
         simplify();
     }
+
+
     public void mult(Fraction fraction){
        this.numerator=this.numerator*fraction.numerator;
        this.denominator=this.denominator*fraction.denominator;
         simplify();
     }
+
+    
     public void div(Fraction fraction){
         this.numerator=this.numerator*fraction.denominator;
         this.denominator=this.denominator*fraction.numerator;
         simplify();
     }
+
+   
     private void simplify(){
         int gcd=gcd(numerator,denominator);
         numerator/=gcd;
         denominator/=gcd;
     }
 
+    /**
+     * Calculates the greatest common divisor (GCD) of two integers.
+     */
     private int gcd(int a, int b){
         while(b!=0){
+
             int temp=b;
             b=a%b;
             a=temp;
--- OOP_LAB/Assignment_2/TestFraction.java
+++ OOP_LAB/Assignment_2/TestFraction.java
@@ -1,4 +1,8 @@
 public class TestFraction {
+    /**
+     * this main method creates two Fraction objects, performs arithmetic operations on them,
+     * and prints the results.
+     */
     public static void main(String[] args) {
      Fraction f1=new Fraction(1,4);
      Fraction f2=new Fraction(3,5);
@@ -6,20 +10,20 @@ public class TestFraction {
         System.out.println("Fraction 1: "+f1);
         System.out.println("Fraction 2: "+f2);
 
-       f1.add(f2);
-        System.out.println("Addition: "+f1);
+        f1.add(f2);
+        System.out.println("The Addition: "+f1);
 
         f1= new Fraction(1,4);
         f1.sub(f2);
-        System.out.println("Substraction: "+f1);
-
+        System.out.println("The Substraction: "+f1);
+        
         f1= new Fraction(1,4);
         f1.mult(f2);
-        System.out.println("Multiplication: "+f1);
+        System.out.println("The Multiplication: "+f1);
 
         f1= new Fraction(1,4);
         f1.div(f2);
-        System.out.println("Division: "+f1);
+        System.out.println("The Division: "+f1);
 
     }
 }
